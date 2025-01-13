a3649a645a933db9c5b8dbc120d7881bf6b3c0a2
HyperVaz <timvas14@gmail.com>
2025-01-13 15:35:34 +0300

    Add functional to add image with status

--- resources/js/views/user/Personal.vue
+++ resources/js/views/user/Personal.vue
@@ -7,6 +7,19 @@
             <textarea v-model="content" class="w-96 mb-3 rounded-3xl border p-2 border-slate-300"
                       placeholder=""></textarea>
         </div>
+        <div class="flex mb-3 items-center">
+            <div>
+                <input @change="storeImage" ref="file" type="file" class="hidden">
+                <a href="#" class="block p-2 w-32 h-10 text-center rounded-3xl bg-green-600 text-white hover:bg-white hover:border hover:border-green-600 hover:text-green-600 box-border" @click.prevent="selectFile()">Image</a>
+            </div>
+            <div>
+                <a @click.prevent="image = null" href="#">Cancel</a>
+            </div>
+
+        </div>
+            <div v-if="image">
+                <img :src="image.url" alt="preview">
+            </div>
         <div>
             <a href="#"
                class="block p-2 w-32 h-10 text-center rounded-3xl bg-green-600 text-white hover:bg-white hover:border hover:border-green-600 hover:text-green-600 box-border">Publish</a>
@@ -22,9 +35,29 @@ export default {
     name: "Personal",
     data() {
         return {
-
+            title: '',
+            content: '',
+            image: null,
         }
     },
+    methods:{
+        selectFile() {
+            this.fileInput = this.$refs.file;
+            this.fileInput.click();
+        },
+        storeImage(e) {
+            const file = e.target.files[0]
+            const formData = new FormData()
+            formData.append('image', file)
+
+            axios.post('/api/post_image', formData)
+                .then(res => {
+                    this.image = res.data.data;
+                })
+        },
+
+
+    }
 }
 
 </script>
--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/post', [\App\Http\Controllers\PostController::class, 'store']);
+    Route::post('/post_image', [\App\Http\Controllers\PostImageController::class, 'store']);
+
 });
 
