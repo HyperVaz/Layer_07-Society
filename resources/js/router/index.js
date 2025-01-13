551c23b4033d2d85be0cda6de42970c99a585e79
HyperVaz <timvas14@gmail.com>
2025-01-13 12:39:11 +0300

    Add login and registration without post component(Sanctum)

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -10,8 +10,50 @@ const router = createRouter({
         {
             path: '/page',
             component: () => import('../views/Page.vue')
-        }
+        },
+        {
+            path: '/user/login',
+            component: () => import('../views/user/Login.vue'),
+            name: 'user.login'
+        },
+        {
+            path: '/user/registration',
+            component: () => import('../views/user/Registration.vue'),
+            name: 'user.registration'
+        },
+        {
+            path: '/user/personal',
+            component: () => import('../views/user/Personal.vue'),
+            name: 'user.personal'
+        },
+
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    axios.get('/api/user')
+        .catch(e => {
+            if (e.response.status === 401) {
+                localStorage.key('x_xsrf_token') ? localStorage.removeItem('x_xsrf_token') : '';
+            }
+        });
+
+    const token = localStorage.getItem('x_xsrf_token');
+
+    if (!token) {
+        if (to.name === 'user.login' || to.name === 'user.registration') {
+            return next();
+        } else {
+            return next({ name: 'user.login' });
+        }
+    }
+
+    if (to.name === 'user.login' || to.name === 'user.registration' && token) {
+        return next({ name: 'user.personal' });
+    }
+
+    next();
+});
+
 
 export default router;
---
+++ resources/js/views/user/Registration.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <div class="w-96 mx-auto">
+
+            <div>
+                <input v-model="name" type="name" placeholder="name"
+                       class="w-96 p-1 mb-2 border border-inherit rounded-lg">
+            </div>
+            <div>
+                <input v-model="email" type="email" placeholder="email"
+                       class="w-96 p-1 mb-2 border border-inherit rounded-lg">
+            </div>
+            <div>
+                <input v-model="password" type="password" placeholder="password"
+                       class="w-96 p-1 mb-2 border border-inherit rounded-lg">
+            </div>
+            <div>
+                <input v-model="password_confirmation" type="password" placeholder="password_confirmation"
+                       class="w-96 p-1 mb-2 border border-inherit rounded-lg">
+            </div>
+            <input @click.prevent="register" type="submit" value="register"
+                   class="border-amber-500 border-2 block float-right mx-auto w-32 p-1 bg-sky-400 text-white rounded-lg">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Registration",
+
+    data() {
+        return {
+            name: null,
+            email: null,
+            password: null,
+            password_confirmation: null,
+        }
+    },
+
+    methods: {
+        register() {
+            axios.get('/sanctum/csrf-cookie')
+                .then(() => {
+                    axios.post('/register', {
+                        name: this.name,
+                        email: this.email,
+                        password: this.password,
+                        password_confirmation: this.password_confirmation
+                    })
+                        .then( res => {
+                            localStorage.setItem('x_xsrf_token', res.headers['x-xsrf-token']);
+                            this.$router.push({name: 'user.personal'})
+                        })
+                        .catch(error => {
+                            // Обработка ошибки при регистрации
+                            console.error("Registration failed:", error);
+                            //Можно показать пользователю сообщение об ошибке
+                        });
+                })
+                .catch(error => {
+                    // Обработка ошибки при получении куки
+                    console.error("Error getting csrf-cookie:", error);
+                    // Можно показать пользователю сообщение об ошибке
+                });
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
