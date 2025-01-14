5342944647446d8498cdb2fa39905de54e839709
HyperVaz <timvas14@gmail.com>
2025-01-14 18:55:32 +0300

    Add functional еto subscribe(following) to users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\User\UserResource;
+use App\Models\SubscriberFollowing;
 use App\Models\User;
 
 class UserController extends Controller
@@ -9,12 +10,28 @@ class UserController extends Controller
     public function index()
     {
         $users = User::whereNot('id', auth()->id())->get();
+        $followingIds = SubscriberFollowing::where('subscriber_id', auth()->id())
+            ->get('following_id')->pluck('following_id')->toArray();
+
+        foreach ($users as $user) {
+            if (in_array($user->id, $followingIds)) {
+                $user->is_followed = true;
+            }
+        }
+
         return UserResource::collection($users);
     }
 
     public function post(User $user)
     {
         return \App\Http\Resources\Post\PostResource::collection($user->posts);
+    }
+    public function toggleFollowing(User $user)
+    {
+        $res = auth()->user()->followings()->toggle($user->id);
+        $data['is_followed'] = count($res['attached']) > 0;
+        return $data;
+
     }
 }
 
